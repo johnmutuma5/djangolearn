ad3bb8a538c37996c3e0dc836fdaf910ee83ca30
John Mutuma <johnmutuma5@gmail.com>
2018-04-23 04:15:52 +0300

    [ft-add-models] add the Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Post(models.Model):
+  title = models.CharField(max_length=25)
+  body = models.TextField()
+  author_id = models.IntegerField()
+
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+  def __str__(self):
+    return self.title
